2a88c2c8da2e70604bf94b2515575303efcf6d0e
Fangrui Song <i@maskray.me>
2016-10-28 13:45:30 -0700

    唐老师加油

--- api.py
+++ api.py
@@ -1,41 +1,63 @@
 #!/usr/bin/env python3
-from flask import Flask, request
-import chardet, email, os, re, subprocess
+from flask import Flask, request, Response
+import chardet, email.utils, json, os, re, subprocess
 
 app = Flask(__name__)
 email_dir = '/home/user/mail'
 
 
-def extract_text(path):
+def show(path):
+    ret = {}
     with open(path, 'rb') as fp:
         buf = fp.read()
         detect = chardet.detect(buf)
         message = email.message_from_string(buf.decode(detect['encoding']))
         for part in message.walk():
             if part.get_content_type() == 'text/plain':
-                return part.get_payload(decode=True).decode(part.get_content_charset() or 'utf-8', 'ignore')
-        return ''
+                for k, v in message.items():
+                    k = k.lower()
+                    if k == 'date':
+                        ret['date'] = email.utils.mktime_tz(email.utils.parsedate_tz(v))
+                    elif k in ['from', 'to']:
+                        name, address = email.utils.parseaddr(v)
+                        ret[k] = name or address
+                    elif k == 'subject':
+                        ret['subject'] = v
+                ret['body'] = part.get_payload(decode=True).decode(part.get_content_charset() or 'utf-8', 'ignore')
+    return ret
 
 
 @app.route('/api/query')
 def api_query():
     args = request.args
     try:
-        argv = ['notmuch', 'search', '--output=files', '--']
+        argv = ['notmuch', 'search', '--output=files']
+        for i in ['offset', 'limit']:
+            if i in args:
+                argv.append('--{}={}'.format(i, args[i]))
+            elif i == 'limit':
+                argv.append('--{}=10'.format(i))
+        argv.append('--')
         for i in ['from', 'to', 'subject', 'id', 'attachment', 'mimetype', 'thread', 'date']:
             if i in args:
                 argv.append(i+':'+args[i])
         if 'term' in args:
             argv.append(args['term'])
         output = subprocess.check_output(argv).decode()
-        return '\n'.join(re.sub('.*/', '', filename) for filename in output.split('\n'))
+        ret = []
+        for filename in output.split('\n'):
+            if len(filename):
+                ret.append(show(filename))
+        return Response(json.dumps(ret), mimetype='application/json')
+        #return '\n'.join(re.sub('.*/', '', filename) for filename in output.split('\n'))
     except subprocess.CalledProcessError:
         raise
 
 
 @app.route('/api/email/<id>')
 def api_email_show(id):
-    return extract_text(os.path.join(email_dir, 'cur', id))
+    return Response(json.dumps(show(os.path.join(email_dir, 'cur', id))),
+                    mimetype='application/json')
 
 
 if __name__ == '__main__':
